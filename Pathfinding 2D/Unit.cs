1dabc7d347ebb12f19a282a62f852d67fef6bb5c
Sebastian Lague <seblague@yahoo.com>
2015-07-06 00:01:24 +0200

    A* Pathfinding in Unity2D

---
+++ Pathfinding 2D/Unit.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+	
+	public Transform target;
+	public float speed = 20;
+
+	Vector2[] path;
+	int targetIndex;
+
+
+	void Start() {
+		path = Pathfinding.RequestPath (transform.position, target.position);
+		StopCoroutine("FollowPath");
+		StartCoroutine("FollowPath");
+	}
+
+	IEnumerator FollowPath() {
+		Vector2 currentWaypoint = path[0];
+
+		while (true) {
+			if ((Vector2)transform.position == currentWaypoint) {
+				targetIndex ++;
+				if (targetIndex >= path.Length) {
+					yield break;
+				}
+				currentWaypoint = path[targetIndex];
+			}
+
+			transform.position = Vector2.MoveTowards(transform.position,currentWaypoint,speed * Time.deltaTime);
+			yield return null;
+
+		}
+	}
+
+	public void OnDrawGizmos() {
+		if (path != null) {
+			for (int i = targetIndex; i < path.Length; i ++) {
+				Gizmos.color = Color.black;
+				//Gizmos.DrawCube((Vector3)path[i], Vector3.one *.5f);
+
+				if (i == targetIndex) {
+					Gizmos.DrawLine(transform.position, path[i]);
+				}
+				else {
+					Gizmos.DrawLine(path[i-1],path[i]);
+				}
+			}
+		}
+	}
+}
