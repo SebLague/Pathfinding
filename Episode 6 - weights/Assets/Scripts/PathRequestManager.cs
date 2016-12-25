e8f8aae59913ebf4d983fde3a07fad6751dfa202
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2016-12-25 14:52:29 +0200

    added ep06

---
+++ Episode 6 - weights/Assets/Scripts/PathRequestManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PathRequestManager : MonoBehaviour {
+
+	Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
+	PathRequest currentPathRequest;
+
+	static PathRequestManager instance;
+	Pathfinding pathfinding;
+
+	bool isProcessingPath;
+
+	void Awake() {
+		instance = this;
+		pathfinding = GetComponent<Pathfinding>();
+	}
+
+	public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback) {
+		PathRequest newRequest = new PathRequest(pathStart,pathEnd,callback);
+		instance.pathRequestQueue.Enqueue(newRequest);
+		instance.TryProcessNext();
+	}
+
+	void TryProcessNext() {
+		if (!isProcessingPath && pathRequestQueue.Count > 0) {
+			currentPathRequest = pathRequestQueue.Dequeue();
+			isProcessingPath = true;
+			pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+		}
+	}
+
+	public void FinishedProcessingPath(Vector3[] path, bool success) {
+		currentPathRequest.callback(path,success);
+		isProcessingPath = false;
+		TryProcessNext();
+	}
+
+	struct PathRequest {
+		public Vector3 pathStart;
+		public Vector3 pathEnd;
+		public Action<Vector3[], bool> callback;
+
+		public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback) {
+			pathStart = _start;
+			pathEnd = _end;
+			callback = _callback;
+		}
+
+	}
+}
